5f84fe2ea0d4b3be8be57c9c095d14eec036d638
cacheUp <bradb26@gmail.com>
2019-09-12 11:33:13 +0300

    test create

--- resources/users/userController.js
+++ resources/users/userController.js
@@ -1,13 +1,20 @@
 const go = require('../utils/crud');
 
 exports.createUser = async (req, res) => {
-  try {
-    const response = await go.createOne('users', 'id', req.body);
-    res
-      .status(201)
-      .json({ message: 'Account successfully created!', response });
-  } catch (err) {
-    console.error(err);
-    res.status(400).json({ message: "Couldn't create account", err });
-  }
+  //   try {
+  //     const response = await go.createOne('users', 'id', req.body);
+  //     res
+  //       .status(201)
+  //       .json({ message: 'Account successfully created!', response });
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(400).json({ message: "Couldn't create account", err });
+  //   }
+  go.createOne('users', 'id', req.body)
+    .then(response =>
+      res
+        .status(201)
+        .json({ message: 'Account successfully created!', response })
+    )
+    .catch(err => console.error(err));
 };
