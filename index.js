73a58f1b2ff67a8deefcdc21490bc3ab018a9865
cacheUp <bradb26@gmail.com>
2019-09-12 08:45:40 +0300

    test route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const db = require('./data/dbConfig');
 const server = express();
 
 // ******************* MIDDLEWARE **************************
@@ -12,6 +12,22 @@ server.use(morgan('dev'));
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+//******* TEST ROUTE */
+
+server.get('/api/users', (req, res) => {
+  db.select('*')
+    .from('users')
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ message: 'Could not get all users from the DB' });
+    });
+});
 
 // ************************ PORT SET UP *************************
 
