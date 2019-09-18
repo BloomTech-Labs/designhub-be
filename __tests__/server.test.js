35c5a70cfc341bf4e3655b0e293ede59bf93e061
cecil john tantay <cjbtantay@gmail.com>
2019-09-18 15:02:16 -0700

    fixing trvis

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,8 +3,9 @@ const server = require('../server');
 
 // initial test
 describe('/ Test route', () => {
-  it('• should return status 200', async () => {
+  it('• should return status 200', async done => {
     const res = await request(server).get('/');
     expect(res.status).toBe(200);
+    done();
   });
 });
