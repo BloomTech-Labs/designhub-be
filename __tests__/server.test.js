939bbb33ec24ac1d9b2a202f76e9c56696b0306e
cecil john tantay <cjbtantay@gmail.com>
2019-09-18 15:07:03 -0700

    use promises for testing

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,9 +3,11 @@ const server = require('../server');
 
 // initial test
 describe('/ Test route', () => {
-  it('• should return status 200', async done => {
-    const res = await request(server).get('/');
-    expect(res.status).toBe(200);
-    done();
+  it('• should return status 200', () => {
+    return request(server)
+      .get('/')
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
   });
 });
