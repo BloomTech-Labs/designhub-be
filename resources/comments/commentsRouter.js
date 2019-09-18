6144af2a2c24a73b83042675cb349aba70691f0e
cacheUp <bradb26@gmail.com>
2019-09-18 11:03:04 +0300

    create photo comment in router

--- resources/comments/commentsRouter.js
+++ resources/comments/commentsRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const commentsController = require('./commentsController');
 
 // ********************* PHOTO COMMENTS *************************
 //***************************************************************
+
+router.post('/photo', commentsController.createPhotoComment);
 
 // ********************* PROJECT COMMENTS *************************
 // ****************************************************************
