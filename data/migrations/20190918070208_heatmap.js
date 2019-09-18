bc996250e7619eea2ea31f6583f2138ee93d391e
cacheUp <bradb26@gmail.com>
2019-09-18 09:37:43 +0300

    bug fix

--- data/migrations/20190918070208_heatmap.js
+++ data/migrations/20190918070208_heatmap.js
@@ -16,7 +16,7 @@ exports.up = function(knex) {
       .onDelete('CASCADE');
 
     tbl.integer('count').defaultTo(1);
-    tbl.date(`${Date.now()})`);
+    tbl.date(`${Date.now()}`);
 
     tbl.text('contribution').defaultTo(null);
   });
