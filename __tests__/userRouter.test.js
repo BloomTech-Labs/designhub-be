1e1e61b1acc00359e1d00bc01c7766e149f27ae2
cecil john tantay <cjbtantay@gmail.com>
2019-10-02 00:31:28 -0700

    test post/createuser

--- __tests__/userRouter.test.js
+++ __tests__/userRouter.test.js
@@ -5,7 +5,38 @@ const ENDPOINT = '/api/v1/users';
 
 describe('userRouter', () => {
   describe('POST / createUser', () => {
-    it('', () => {});
+    it('should return 422 if sub is not in body', () => {
+      return request(server)
+        .post(`${ENDPOINT}/`)
+        .send({})
+        .then(res => {
+          expect(res.status).toBe(422);
+        });
+    });
+
+    beforeEach(() => {
+      return request(server)
+        .post(`${ENDPOINT}/`)
+        .send({ sub: 'auth0|5d83b8d3d8e1cf0df49647e3' });
+    });
+
+    it('should return 200 if User already created', () => {
+      return request(server)
+        .post(`${ENDPOINT}/`)
+        .send({ sub: 'auth0|5d83b8d3d8e1cf0df49647e3' })
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it('should return 201 if User newly is created', () => {
+      return request(server)
+        .post(`${ENDPOINT}/`)
+        .send({ sub: '1337' })
+        .then(res => {
+          expect(res.status).toBe(201);
+        });
+    });
   });
 
   describe('GET / getUserById', () => {
--- resources/users/userController.js
+++ resources/users/userController.js
@@ -9,33 +9,33 @@ exports.createUser = async (req, res) => {
 
   if (!sub) {
     res.status(422).json({ message: 'Missing sub field' });
-  }
+  } else {
+    try {
+      user = await db('users')
+        .select('*')
+        .where('auth0Id', sub);
 
-  try {
-    user = await db('users')
-      .select('*')
-      .where('auth0Id', sub);
+      if (user.length > 0) {
+        res.status(200).json({ message: 'User already created', user });
+      } else {
+        if (req.body.picture) {
+          avatar = req.body.picture;
+        }
+        let userObject = {
+          auth0Id: sub,
+          avatar: avatar
+        };
 
-    if (user.length > 0) {
-      res.status(200).json({ message: 'User already created', user });
-    } else {
-      if (req.body.picture) {
-        avatar = req.body.picture;
+        const [id] = await go.createOne('users', 'id', userObject);
+        const user = await go.getById('users', id);
+        res
+          .status(201)
+          .json({ message: 'Account successfully created!', user });
       }
-      let userObject = {
-        auth0Id: sub,
-        avatar: avatar
-      };
-
-      const [id] = await go.createOne('users', 'id', userObject);
-      const user = await go.getById('users', id);
-      res.status(201).json({ message: 'Account successfully created!', user });
+    } catch ({ message }) {
+      res.status(500).json({ message: 'Something went wrong', error: message });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(400).message({ message: 'bad request' });
   }
-  console.log(user);
 };
 
 exports.getUserById = async (req, res) => {
