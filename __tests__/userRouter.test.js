3780cd36d6d2b9fea3f28f072abcc22e4baefa40
cecil john tantay <cjbtantay@gmail.com>
2019-10-04 10:51:11 -0700

    switch to async and await

--- __tests__/userRouter.test.js
+++ __tests__/userRouter.test.js
@@ -6,135 +6,107 @@ const ENDPOINT = '/api/v1/users';
 
 describe('userRouter', () => {
   describe('POST / createUser', () => {
-    it('should return 422 if sub is not in body', () => {
-      return request(server)
+    it('should return 422 if sub is not in body', async () => {
+      const res = await request(server)
         .post(`${ENDPOINT}/`)
-        .send({})
-        .then(res => {
-          expect(res.status).toBe(422);
-        });
+        .send({});
+
+      expect(res.status).toBe(422);
     });
 
-    beforeEach(() => {
-      return request(server)
+    beforeEach(async () => {
+      await request(server)
         .post(`${ENDPOINT}/`)
         .send({ sub: 'auth0|5d83b8d3d8e1cf0df49647e3' });
     });
 
-    it('should return 200 if User already created', () => {
-      return request(server)
+    it('should return 200 if User already created', async () => {
+      const res = await request(server)
         .post(`${ENDPOINT}/`)
-        .send({ sub: 'auth0|5d83b8d3d8e1cf0df49647e3' })
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
+        .send({ sub: 'auth0|5d83b8d3d8e1cf0df49647e3' });
+      expect(res.status).toBe(200);
     });
 
-    it('should return 201 if User newly is created', () => {
+    it('should return 201 if User newly is created', async () => {
       const randomNum = Math.random();
-      return request(server)
+      const res = await request(server)
         .post(`${ENDPOINT}/`)
-        .send({ sub: `${randomNum}` })
-        .then(res => {
-          expect(res.status).toBe(201);
-        });
+        .send({ sub: `${randomNum}` });
+
+      expect(res.status).toBe(201);
     });
   });
 
   describe('GET /:id getUserById', () => {
-    it('should return 200 OK', () => {
-      return request(server)
-        .get(`${ENDPOINT}/1`)
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
+    it('should return 200 OK', async () => {
+      const res = await request(server).get(`${ENDPOINT}/1`);
+      expect(res.status).toBe(200);
     });
 
-    it('should return 400 if id is not in db', () => {
-      return request(server)
-        .get(`${ENDPOINT}/1337`)
-        .then(res => {
-          expect(res.status).toBe(404);
-        });
+    it('should return 400 if id is not in db', async () => {
+      const res = await request(server).get(`${ENDPOINT}/1337`);
+
+      expect(res.status).toBe(404);
     });
   });
 
   describe('GET /check/:username getUserByUsername', () => {
-    it('should return 200', () => {
-      return request(server)
-        .get(`${ENDPOINT}/check/eriklambert`)
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
+    it('should return 200', async () => {
+      const res = await request(server).get(`${ENDPOINT}/check/eriklambert`);
+      expect(res.status).toBe(200);
     });
 
-    it('should return 204 when user does not exist', () => {
-      return request(server)
-        .get(`${ENDPOINT}/check/0248`)
-        .then(res => {
-          expect(res.status).toBe(204);
-        });
+    it('should return 204 when user does not exist', async () => {
+      const res = await request(server).get(`${ENDPOINT}/check/0248`);
+
+      expect(res.status).toBe(204);
     });
   });
 
   describe('GET / getAllUsers', () => {
-    it('should return 200', () => {
-      return request(server)
-        .get(`${ENDPOINT}/`)
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
+    it('should return 200', async () => {
+      const res = await request(server).get(`${ENDPOINT}/`);
+      expect(res.status).toBe(200);
     });
   });
 
   describe('PUT /:id updateUserById', () => {
-    it('should return 200', () => {
-      return request(server)
+    it('should return 200', async () => {
+      const res = await request(server)
         .put(`${ENDPOINT}/1`)
-        .send({ auth0Id: 'auth0|5d83b8d3d8e1cf0df49647e3' })
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
+        .send({ auth0Id: 'auth0|5d83b8d3d8e1cf0df49647e3' });
+      expect(res.status).toBe(200);
     });
 
-    it('should return 422 for missing fields', () => {
-      return request(server)
+    it('should return 422 for missing fields', async () => {
+      const res = await request(server)
         .put(`${ENDPOINT}/1`)
-        .send({})
-        .then(res => {
-          expect(res.status).toBe(422);
-        });
+        .send({});
+
+      expect(res.status).toBe(422);
     });
 
-    it('should return 400 for user not in db', () => {
-      return request(server)
+    it('should return 400 for user not in db', async () => {
+      const res = await request(server)
         .put(`${ENDPOINT}/439734`)
-        .send({ auth0Id: 'auth0|5d83b8d3d8e1cf0df49647e3' })
-        .then(res => {
-          expect(res.status).toBe(404);
-        });
+        .send({ auth0Id: 'auth0|5d83b8d3d8e1cf0df49647e3' });
+
+      expect(res.status).toBe(404);
     });
   });
 
   describe('DELETE /:id deleteUserById', () => {
-    it('should return 200', () => {
-      return go
-        .createOne('users', 'id', { auth0Id: 'testUser' })
-        .then(([id]) => {
-          return request(server)
-            .delete(`${ENDPOINT}/${id}`)
-            .then(res => {
-              expect(res.status).toBe(200);
-            });
-        });
+    it('should return 200', async () => {
+      const [id] = await go.createOne('users', 'id', { auth0Id: 'testUser' });
+      const res = await request(server).delete(`${ENDPOINT}/${id}`);
+
+      expect(res.status).toBe(200);
     });
 
-    it('should return 404 if id does not exist', () => {
-      return request(server)
-        .delete(`${ENDPOINT}/${4555}`)
-        .then(res => {
-          expect(res.status).toBe(404);
-        });
+    it('should return 404 if id does not exist', async () => {
+      const res = await request(server).delete(`${ENDPOINT}/${4555}`);
+
+      expect(res.status).toBe(404);
     });
   });
 });
