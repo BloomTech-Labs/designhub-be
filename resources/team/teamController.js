1deed521b735c0ac6138bb2d03f27a73de4f31d5
cacheUp <bradb26@gmail.com>
2019-10-08 10:20:55 +0300

    get all teams

--- resources/team/teamController.js
+++ resources/team/teamController.js
@@ -32,3 +32,38 @@ exports.getTeamById = async (req, res) => {
     res.status(400).json({ message: 'Couldnt find team', message });
   }
 };
+
+exports.getAllTeams = async (req, res) => {
+  try {
+    const data = go.getMany('team');
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Couldnt teams' });
+  }
+};
+
+// exports.deleteStar = async (req, res) => {
+//   if (!req.body.id) {
+//     res
+//       .status(400)
+//       .json({ message: 'userId was not attached to the req.body' });
+//   }
+
+//   if (!req.params.id) {
+//     res
+//       .status(400)
+//       .json({ message: 'projectId was not attached to the req.params' });
+//   }
+
+//   try {
+//     await db('starred_projects')
+//       .del()
+//       .where('projectId', req.params.id)
+//       .andWhere('userId', req.body.id);
+//     res.status(200).json({ message: 'Star successfully deleted' });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).json({ message: "Couldn't delete star.", error: error });
+//   }
+// };
