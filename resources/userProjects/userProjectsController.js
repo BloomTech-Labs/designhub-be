f8307999cf955f6efca33ae2381b63511efc34fe
cecil john tantay <cjbtantay@gmail.com>
2019-10-08 22:32:04 -0700

    add error handling to userProjectsController

--- resources/userProjects/userProjectsController.js
+++ resources/userProjects/userProjectsController.js
@@ -2,16 +2,21 @@ const go = require('../utils/crud');
 const db = require('../../data/dbConfig');
 
 exports.createProject = async (req, res) => {
-  try {
-    const [id] = await go.createOne('user_projects', 'id', req.body);
-    const data = await go.getById('user_projects', id);
-    res
-      .status(201)
-      .json({ message: 'Project successfully created!', data, id });
-  } catch ({ message }) {
-    res
-      .status(400)
-      .json({ message: "Couldn't create project", error: message });
+  const { userId, name } = req.body;
+  if (!userId || !name) {
+    res.status(422).json({ message: 'Missing fields' });
+  } else {
+    try {
+      const [id] = await go.createOne('user_projects', 'id', req.body);
+      const data = await go.getById('user_projects', id);
+      res
+        .status(201)
+        .json({ message: 'Project successfully created!', data, id });
+    } catch ({ message }) {
+      res
+        .status(500)
+        .json({ message: "Couldn't create project", error: message });
+    }
   }
 };
 
@@ -22,25 +27,30 @@ exports.getProjectById = async (req, res) => {
       .select('up.*', 'u.username')
       .where('up.id', id)
       .innerJoin('users as u', 'up.userId', '=', 'u.id');
-    res.status(200).json(data);
+    if (data.length === 0) {
+      res.status(404).json({ message: 'project not found' });
+    } else {
+      res.status(200).json(data);
+    }
   } catch ({ message }) {
-    res.status(400).json({ message: "Couldn't find project.", error: message });
+    res.status(500).json({ message: "Couldn't find project.", error: message });
   }
 };
 
 exports.getProjectByUserId = async (req, res) => {
-  console.log('hrello world!');
   const { userId } = req.params;
-  console.log(userId);
   try {
     const data = await go
       .getByUserId('user_projects as up', userId, 'up.*', 'u.username')
       .innerJoin('users as u', 'up.userId', '=', 'u.id');
-    console.log(data);
-    res.status(200).json(data);
+    if (data.length === 0) {
+      res.status(404).json({ message: 'user id not found' });
+    } else {
+      res.status(200).json(data);
+    }
   } catch ({ message }) {
     res
-      .status(400)
+      .status(500)
       .json({ message: "Couldn't get projects by user.", error: message });
   }
 };
@@ -49,16 +59,19 @@ exports.getProjectByUserId = async (req, res) => {
 
 exports.getRecentProjectByUserId = async (req, res) => {
   const { userId } = req.params;
-  console.log(userId);
   try {
     const data = await go
       .getByUserId('user_projects', userId)
       .orderBy('created_at', 'desc')
       .limit(8);
-    res.status(200).json(data);
+    if (data.length === 0) {
+      res.status(404).json({ message: 'user id not found' });
+    } else {
+      res.status(200).json(data);
+    }
   } catch ({ message }) {
     res
-      .status(400)
+      .status(500)
       .json({ message: "Couldn't get projects by user.", error: message });
   }
 };
@@ -84,43 +97,62 @@ exports.getAllProjects = async (req, res) => {
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(400).json({ message: "Couldn't get projects.", error: error });
+    res.status(500).json({ message: "Couldn't get projects.", error: error });
   }
 };
 
 exports.getProjectsByName = async (req, res) => {
   const term = req.body.projectName;
 
-  try {
-    const data = await db('user_projects').where(
-      'projectName',
-      'like',
-      `%${term}%`
-    );
+  if (!req.body.projectName) {
+    res.status(422).json({ message: 'missing fields' });
+  } else {
+    try {
+      const data = await db('user_projects').where('name', 'like', `%${term}%`);
 
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: "Couldn't find project.", error: error });
+      if (data.length === 0) {
+        res.status(404).json({ message: 'project with name not found' });
+      } else {
+        res.status(200).json(data);
+      }
+    } catch ({ message }) {
+      res.status(500).json(message);
+    }
   }
 };
 
 exports.updateProjectById = async (req, res) => {
   const { id } = req.params;
-  try {
-    await go.updateById('user_projects', req.body, id);
-    const data = await go.getById('user_projects', id);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json({ message: "Couldn't update project.", error: error });
+  const { userId, name } = req.body;
+
+  if (!userId || !name) {
+    res.status(422).json({ message: 'missing fields' });
+  } else {
+    try {
+      await go.updateById('user_projects', req.body, id);
+      const data = await go.getById('user_projects', id);
+      if (data.length === 0) {
+        res.status(404).json({ message: 'project id not found' });
+      } else {
+        res.status(200).json(data);
+      }
+    } catch ({ message }) {
+      res
+        .status(500)
+        .json({ message: "Couldn't update project.", error: message });
+    }
   }
 };
 
 exports.deleteProjectById = async (req, res) => {
   const { id } = req.params;
   try {
-    await go.destroyById('user_projects', id);
-    res.status(200).json({ message: 'User successfully deleted' });
+    const deleted = await go.destroyById('user_projects', id);
+    if (deleted !== 1) {
+      res.status(404).json({ message: 'user id not found' });
+    } else {
+      res.status(200).json({ message: 'User successfully deleted' });
+    }
   } catch (error) {
     res.status(400).json({ message: "Couldn't delete user.", error: error });
   }
