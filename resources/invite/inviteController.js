c10cf60f5c40a5ff2a3fdb9943193e34ae565904
cacheUp <bradb26@gmail.com>
2019-10-09 12:45:45 +0300

    error helper

--- resources/invite/inviteController.js
+++ resources/invite/inviteController.js
@@ -1,3 +1,11 @@
+const errorHelper = (res, condition, item) => {
+  if (!condition) {
+    res
+      .status(400)
+      .json({ message: `${item} was not attatched to the req.body` });
+  }
+};
+
 exports.getInvitesByUserId = (req, res) => {};
 
 exports.getInviteCountByUserId = (req, res) => {};
@@ -8,7 +16,27 @@ exports.createFollowInvite = (req, res) => {};
 
 exports.createStarredInvite = (req, res) => {};
 
-exports.createCommentsInvite = (req, res) => {};
+exports.createCommentsInvite = (req, res) => {
+  const {
+    username,
+    commentText,
+    type,
+    projectId,
+    invitedUserId,
+    activeUserId,
+    mainImgUrl,
+    commentsId,
+    activeUserAvatar
+  } = req.body;
+  errorHelper(res, username, 'username');
+  errorHelper(res, commentText, 'commentText');
+  errorHelper(res, projectId, 'projectId');
+  errorHelper(res, invitedUserId, 'invitedUserId');
+  errorHelper(res, activeUserId, 'activeUserId');
+  errorHelper(res, mainImgUrl, 'mainImgUrl');
+  errorHelper(res, commentsId, 'commentsId');
+  errorHelper(res, activeUserAvatar, 'activeUserAvatar');
+};
 
 exports.deleteInviteById = (req, res) => {};
 
