7ab4877854ad3aeb5f5a4c0e545b431a96c4365b
cacheUp <bradb26@gmail.com>
2019-10-14 10:51:45 +0300

    bug fix

--- resources/explore/exploreRouter.js
+++ resources/explore/exploreRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const exploreController = require('./exploreController');
 
-router.get('/explore', exploreController.getExploreOptions);
+router.get('/', exploreController.getExploreOptions);
 
 module.exports = router;
