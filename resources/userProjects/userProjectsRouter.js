f56c823fc29917c54df25df8f091cb87ba93e606
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-11-11 15:23:53 -0800

    Attempted to secure user project routes

--- resources/userProjects/userProjectsRouter.js
+++ resources/userProjects/userProjectsRouter.js
@@ -4,13 +4,13 @@ const projectController = require('./userProjectsController');
 
 const secured = require('../utils/secured');
 
-router.post('/', projectController.createProject);
+router.post('/', secured, projectController.createProject);
 router.get('/:id', secured, projectController.getProjectById);
 router.get('/', projectController.getAllProjects);
-router.get('/users/:userId/', projectController.getProjectByUserId);
-router.get('/recent/:userId/', projectController.getRecentProjectByUserId);
+router.get('/users/:userId/', secured, projectController.getProjectByUserId);
+router.get('/recent/:userId/', secured, projectController.getRecentProjectByUserId);
 router.post('/name', projectController.getProjectsByName);
-router.put('/:id', projectController.updateProjectById);
-router.delete('/:id', projectController.deleteProjectById);
+router.put('/:id', secured, projectController.updateProjectById);
+router.delete('/:id', secured, projectController.deleteProjectById);
 
 module.exports = router;
