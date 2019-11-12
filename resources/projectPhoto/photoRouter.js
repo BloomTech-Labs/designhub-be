8985a4f4a967bca815019aa4756895f4dc6e451f
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-11-12 17:51:28 -0500

    secured project photos

--- resources/projectPhoto/photoRouter.js
+++ resources/projectPhoto/photoRouter.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const photoController = require('./photoController');
 
+const secured = require('../utils/secured');
+
 router.post('/signed', photoController.signedUrl);
 // router.get('/', photoController.getAllPhotos);
 router.get('/:id', photoController.getPhotosByProjectId);
 router.get('/one/:id', photoController.getPhotoById);
-router.post('/', photoController.createProjectPhoto);
-router.delete('/:id', photoController.deletePhotoById);
+router.post('/', secured, photoController.createProjectPhoto);
+router.delete('/:id', secured, photoController.deletePhotoById);
 
 module.exports = router;
