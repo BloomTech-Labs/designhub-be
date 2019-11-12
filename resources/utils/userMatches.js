09518e35eec8656989acbb4bfc0d328d244280ce
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-11-12 10:36:41 -0800

    Removed extraneous code
    Co-authored-by: amguenoun amguenoun@gmail.com
    Co-authored-by: rogermcconkiejr roger.mcconkiejr@gmail.com
    Co-authored-by: tishaholder tishay_ann@yahoo.com

--- resources/utils/userMatches.js
+++ resources/utils/userMatches.js
@@ -2,7 +2,6 @@ const go = require('./crud');
 
 const userMatches = async (token, userId) => {
   const user = await go.getById('users', userId);
-
   if(user[0] && user[0].auth0Id === token.sub) {
     return true;
   } else return false;
