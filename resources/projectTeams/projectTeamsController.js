75d861196aedf9cb3acd7a39b6fd45777e0c35e5
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-11-19 12:00:30 -0700

    projectTeams contoller and Router created and imported
    Co-authored-by: amguenoun <amguenoun@gmail.com>
    Co-authored-by: tishaholder <tishay_ann@yahoo.com>
    Co-authored-by: mtourj <mtourjoman01@hotmail.com>

---
+++ resources/projectTeams/projectTeamsController.js
@@ -0,0 +1,2 @@
+const go = require('../utils/crud');
+const db = require('../../data/dbConfig');
---
+++ resources/projectTeams/projectTeamsRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.post() // create invitations for project.
+router.get(':/id')  
+
+
+module.exports = router;
