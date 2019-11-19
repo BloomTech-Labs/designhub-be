389176ba37e885058588e20ae253f74ea2a04133
Samantha Egge <samantha.c.egge@gmail.com>
2019-11-19 12:14:08 -0800

    Made modifications to secured middleware, to prevent not using the jwt.

--- resources/utils/secured.js
+++ resources/utils/secured.js
@@ -2,16 +2,8 @@ require('dotenv').config();
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
-const securing = (request, response, next) => {
-  console.log('we in here', process.env.DB_ENV)
- if (process.env.DB_ENV === 'testing') {
-  //  const secured = jwt({sub: 'asdfadsf?'})
-  request.user = {sub: 'auth0|5d83b8d3d8e1cf0df49647e3'}
-  console.log('request.user in securing: ', request.body)
-  // request.user.sub = 
-  next()
- } else {
-   const secured = jwt({
+const secured = (process.env.DB_ENV !== 'testing') ? 
+  jwt({
     // Dynamically provide a signing key
     // based on the kid in the header and 
     // the signing keys provided by the JWKS endpoint.
@@ -26,9 +18,15 @@ const securing = (request, response, next) => {
     audience: process.env.AUDIENCE_URL,
     issuer: `${process.env.AUTH0URI}/`,
     algorithms: ['RS256']
-  })
-  return secured
- }
-};
+    }) :
+    (
+      (request, response, next) => {
+        console.log('we in here', process.env.DB_ENV)
+        //  const secured = jwt({sub: 'asdfadsf?'})
+        // request.user = {sub: 'auth0|5d83b8d3d8e1cf0df49647e3'}
+        // request.user.sub = 
+        next()
+      }
+    )
 
-module.exports = securing;
+module.exports = secured;
