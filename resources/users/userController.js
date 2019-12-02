587b95f5d1e0b3d3440340be51d8d66942b95fda
Ahmad Guenoun <amguenoun@gmail.com>
2019-12-02 16:27:04 -0500

    created endpoint getUserByEmail
    Co-authored-by: rogermcconkiejr <roger.mcconkiejr@gmail.com>
    Co-authored-by: tishaholder <tishay_ann@yahoo.com>

--- resources/users/userController.js
+++ resources/users/userController.js
@@ -129,3 +129,15 @@ exports.deleteUserById = async (req, res) => {
     res.status(500).json({ message: "Couldn't delete user.", error: error });
   }
 };
+
+exports.getUserByEmail = async (req, res) => {
+  const { email } = req.params;
+
+  try {
+    const user = await go.getUserByEmail(email);
+    res.status(200).json(user);
+  }
+  catch (err) {
+    res.status(500).json({ message: "Couldn't get user.", error: error });
+  }
+}
--- resources/users/userRouter.js
+++ resources/users/userRouter.js
@@ -10,5 +10,6 @@ router.get('/check/:username', userController.getUserByUsername);
 router.get('/', userController.getAllUsers);
 router.put('/:id', secured, userController.updateUserById);
 router.delete('/:id', secured, userController.deleteUserById);
+router.get('/mail/:email', userController.getUserByEmail);
 
 module.exports = router;
