18cca10294c2ff86bb238c47f62a6e61a27cdac4
Ahmad Guenoun <amguenoun@gmail.com>
2019-12-18 18:45:22 -0500

    added in secured middle ware to get rid of error

--- resources/userResearch/userResearchRouter.js
+++ resources/userResearch/userResearchRouter.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const userResearchController = require('./userResearchController');
 
+const secured = require('../utils/secured');
+
 router.post('/signed', userResearchController.signedUrl)
 router.get('/:id', userResearchController.getResearchById)
 router.get('/project/:id', userResearchController.getResearchByProjectId)
-router.post('/', userResearchController.createUserResearch)
-router.delete('/:id', userResearchController.deleteUserResearchById)
+router.post('/', secured, userResearchController.createUserResearch)
+router.delete('/:id', secured, userResearchController.deleteUserResearchById)
 
 
 module.exports = router;
