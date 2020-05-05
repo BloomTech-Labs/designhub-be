e52cdcdad01df51628d8b432e80eed623cc7a4bc
JimmyMcBride <mcbride967@gmail.com>
2020-05-04 20:10:28 -0500

    inited apollo server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const go = require('./resources/utils/crud');
+const { ApolloServer, gql } = require('apollo-server-express');
 // middleware imports
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const server = express();
+const app = express();
 // router imports
 const userRouter = require('./resources/users/userRouter');
 const projectRouter = require('./resources/userProjects/userProjectsRouter');
@@ -23,34 +24,70 @@ const projectInvitesRouter = require('./resources/projectInvites/projectInvitesR
 const userResearchRouter = require('./resources/userResearch/userResearchRouter');
 const categoriesRouter = require('./resources/categories/categoriesRouter');
 
+const db = require('./data/dbConfig');
+
 // ***************** MIDDLEWARE **************************
 
-server.use(morgan('dev'));
-server.use(express.json());
-server.use(cors());
-server.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 // ************************ TEST ENDPOINT ************
 
-server.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
   res.send('<h1>She works</h1>');
 });
 
 //******************** Routes *******************************/
 
-server.use('/api/v1/users', userRouter);
-server.use('/api/v1/projects', projectRouter);
-server.use('/api/v1/photo/projects', photoRouter);
-server.use('/api/v1/followers', followersRouter);
-server.use('/api/v1/comments', commentsRouter);
-server.use('/api/v1/heatmap', heatmapRouter);
-server.use('/api/v1/star', starRouter);
-server.use('/api/v1/search', searchBarRouter);
-server.use('/api/v1/team', teamRouter);
-server.use('/api/v1/teamMember', teamMemberRouter);
-server.use('/api/v1/invite', inviteRouter);
-server.use('/api/v1/explore', exploreRouter);
-server.use('/api/v1/projectInvites', projectInvitesRouter);
-server.use('/api/v1/research', userResearchRouter);
-server.use('/api/v1/categories', categoriesRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/projects', projectRouter);
+app.use('/api/v1/photo/projects', photoRouter);
+app.use('/api/v1/followers', followersRouter);
+app.use('/api/v1/comments', commentsRouter);
+app.use('/api/v1/heatmap', heatmapRouter);
+app.use('/api/v1/star', starRouter);
+app.use('/api/v1/search', searchBarRouter);
+app.use('/api/v1/team', teamRouter);
+app.use('/api/v1/teamMember', teamMemberRouter);
+app.use('/api/v1/invite', inviteRouter);
+app.use('/api/v1/explore', exploreRouter);
+app.use('/api/v1/projectInvites', projectInvitesRouter);
+app.use('/api/v1/research', userResearchRouter);
+app.use('/api/v1/categories', categoriesRouter);
 
-module.exports = server;
+const typeDefs = gql`
+  input FullGreetInput {
+    firstName: String!
+    lastName: String!
+  }
+
+  type Query {
+    hello: String!
+    greet(name: String!): String!
+    fullGreet(data: FullGreetInput!): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'Hello, world!';
+    },
+    greet(parent, args, ctx) {
+      return `Hello, ${args.name}!`;
+    },
+    fullGreet(_, { data }) {
+      return `Hello, ${data.firstName} ${data.lastName}! 💖`;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+module.exports = app;
