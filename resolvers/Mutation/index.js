fce512e5ae14954cd5f0bbc749d30d24fb51e7b0
Samantha-Reis <49262294+Samantha-Reis@users.noreply.github.com>
2020-05-18 21:45:17 -0400

    Fixing mutations

--- resolvers/Mutation/index.js
+++ resolvers/Mutation/index.js
@@ -2,283 +2,261 @@ const db = require('../../data/dbConfig');
 const { ValidationError } = require('apollo-server-express');
 
 const Mutation = {
-	async addUser(_, { data }) {
-		return await db('users').insert(data).returning('*');
-	},
+  async addUser(_, { data }) {
+    const user = await db('users').insert(data).returning('*');
+    console.log(user);
+    return user[0];
+  },
 
-	async updateUser(_, { data }) {
-		return await db('users').update(data).returning('*');
-	},
+  async updateUser(_, { data }) {
+    const user = await db('users')
+      .update(data)
+      .where('id', data.id)
+      .returning('*');
+    console.log(user);
+    return user[0];
+  },
 
-	async deleteUser(_, { id }) {
-		const deletedUser = await db('users').del().where('id', id);
-		try {
-			if (!deletedUser) {
-				throw new Error('This user does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async deleteUser(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deletedUser = await db('users').where('id', id).del();
+      if (!deletedUser) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addProject(_, { data }) {
-		return await db('user_projects').insert(data).return('*');
-	},
+  async addProject(_, { data }) {
+    const project = await db('user_projects').insert(data).returning('*');
+    console.log(project);
+    return project[0];
+  },
 
-	async updateProject(_, { data }) {
-		000;
-		return await db('user_projects').update(data).return('*');
-	},
+  async updateProject(_, { data }) {
+    const project = await db('user_projects').update(data).returning('*');
+    console.log(project);
+    return project[0];
+  },
 
-	async deleteProject(_, { id }) {
-		const deletedProject = await db('user_projects').del().where('id', id);
-		try {
-			if (!deletedProject) {
-				throw new Error('This project does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async deleteProject(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deletedProject = await db('user_projects').where('id', id).del();
+      if (!deletedProject) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addProjectPhoto(_, { data }) {
-		return await db('project_photos').insert(data).return('*');
-	},
+  async addProjectPhoto(_, { data }) {
+    const projectPhoto = await db('project_photos').insert(data).returning('*');
+    console.log(projectPhoto);
+    return projectPhoto[0];
+  },
 
-	async updateProjectPhoto(_, { data }) {
-		return await db('project_photos').update(data).return('*');
-	},
+  async updateProjectPhoto(_, { data }) {
+    const projectPhoto = await db('project_photos').update(data).returning('*');
+    console.log(projectPhoto);
+    return projectPhoto[0];
+  },
 
-	async deleteProjectPhotos(_, { id }) {
-		const deletedProjectPhoto = await db('project_photos').del().where('id', id);
-		try {
-			if (!deletedProjectPhoto) {
-				throw new Error('This project does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async deleteProjectPhotos(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deletedProjectPhoto = await db('project_photos')
+        .where('id', id)
+        .del();
+      if (!deletedProjectPhoto) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addComments(_, { data }) {
-		return await db('comments').insert(data).return('*');
-	},
+  async addComments(_, { data }) {
+    const comments = await db('comments').insert(data).returning('*');
+    console.log(comments);
+    return comments[0];
+  },
 
-	async updateComments(_, { data }) {
-		return await db('comments').update(data).return('*');
-	},
+  async updateComments(_, { data }) {
+    const comments = await db('comments').update(data).returning('*');
+    console.log(comments);
+    return comments[0];
+  },
 
-	async deleteComments(_, { id }) {
-		const deletedComments = await db('comments').del().where('id', id);
-		try {
-			if (!deletedComments) {
-				throw new Error('This comment does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async deleteComments(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deletedComments = await db('comments').where('id', id).del();
+      if (!deletedComments) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addPhotoComments(_, { data }) {
-		return await db('comments').insert(data).return('*');
-	},
+  async addFollower(_, { data }) {
+    const followers = await db('user_followers').insert(data).returning('*');
+    console.log(followers);
+    return followers[0];
+  },
 
-	async updatePhotoComments(_, { data }) {
-		return await db('comments').update(data).return('*');
-	},
+  async deleteFollower(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deletedFollower = await db('user_followers').where('id', id).del();
+      if (!deletedFollower) return rej(false);
+      return res(true);
+    });
+  },
 
-	async deletePhotoComments(_, { id }) {
-		const deletedPhotoComments = await db('comments').del().where('id', id);
-		try {
-			if (!deletedPhotoComments) {
-				throw new Error('This photo comment does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async addHeatmap(_, { data }) {
+    const heatmap = await db('heatmap').insert(data).returning('*');
+    console.log(heatmap);
+    return heatmap[0];
+  },
 
-	async addFollower(_, { data }) {
-		return await db('user_followers').insert(data).return('*');
-	},
+  async deleteHeatmap(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deleteHeat = await db('heatmap').where('id', id).del();
+      if (!deleteHeat) return rej(false);
+      return res(true);
+    });
+  },
 
-	async deleteFollower(_, { id }) {
-		const deletedFollower = await db('user_followers').del().where('id', id);
-		try {
-			if (!deletedFollower) {
-				throw new Error('This photo comment does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async addStarred(_, { data }) {
+    const starred = await db('starred_projects').insert(data).returning('*');
+    console.log(starred);
+    return starred[0];
+  },
 
-	async addHeatmap(_, { data }) {
-		return await db('heatmap').insert(data).return('*');
-	},
+  async deleteStarred(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deleteStar = await db('starred_projects').where('id', id).del();
+      if (!deleteStar) return rej(false);
+      return res(true);
+    });
+  },
 
-	async deleteHeatmap(_, { id }) {
-		const deletedHeatmap = await db('comments').del().where('id', id);
-		try {
-			if (!deletedHeatmap) {
-				throw new Error('This heatmap does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async addInvite(_, { data }) {
+    const invite = await db('invite').insert(data).returning('*');
+    console.log(invite);
+    return invite[0];
+  },
 
-	async addStarred(_, { data }) {
-		return await db('starred_projects').insert(data).return('*');
-	},
+  async addInviteFollow(_, { data }) {
+    const invite = await db('invite').insert(data).returning('*');
+    console.log(invite);
+    return invite[0];
+  },
 
-	async deleteStarred(_, { id }) {
-		const deletedStarred = await db('starred_projects').del().where('id', id);
-		try {
-			if (!deletedStarred) {
-				throw new Error('This project does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async addInviteStarred(_, { data }) {
+    const invite = await db('invite').insert(data).returning('*');
+    console.log(invite);
+    return invite[0];
+  },
 
-	async addInvite(_, { data }) {
-		return await db('invite').insert(data).return('*');
-	},
+  async addInviteComments(_, { data }) {
+    const invite = await db('invite').insert(data).returning('*');
+    console.log(invite);
+    return invite[0];
+  },
 
-	async addInviteFollow(_, { data }) {
-		return await db('invite').insert(data).return('*');
-	},
+  async updateInvites(_, { data }) {
+    const invite = await db('invite').update(data).returning('*');
+    console.log(invite);
+    return invite[0];
+  },
 
-	async addInviteStarred(_, { data }) {
-		return await db('invite').insert(data).return('*');
-	},
+  async deleteInvite(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deleteInvite = await db('invite').where('id', id).del();
+      if (!deleteInvite) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addInviteComments(_, { data }) {
-		return await db('invite').insert(data).return('*');
-	},
+  async search(_, { text }) {
+    const projectText = text.toLowerCase();
+    const userText = text.replace(/\s+/g, '').toLowerCase();
+    try {
+      if (!text) throw new ValidationError('Must add text to search!! 🙃');
+      const projects = await db('user_projects')
+        .select('*')
+        .whereRaw(`LOWER(name) LIKE ?`, [`%${projectText}%`])
+        .andWhere('privateProjects', false);
 
-	async updateInvites(_, { data }) {
-		return await db('invite').update(data).return('*');
-	},
+      const users = await db('users')
+        .select('*')
+        .whereRaw(`LOWER(username) LIKE ?`, [`%${userText}%`])
+        .orWhereRaw(`LOWER(CONCAT("firstName", "lastName")) LIKE ?`, [
+          `%${userText}%`,
+        ]);
+      return { projects, users };
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  },
 
-	async deleteInvite(_, { id }) {
-		const deletedInvite = await db('invite').del().where('id', id);
-		try {
-			if (!deletedInvite) {
-				throw new Error('This project does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async addCategory(_, { data }) {
+    const category = await db('project_categories').insert(data).returning('*');
+    console.log(category);
+    return category[0];
+  },
 
-	async search(_, { text }) {
-		const projectText = text.toLowerCase();
-		const userText = text.replace(/\s+/g, '').toLowerCase();
-		try {
-			if (!text) throw new ValidationError('Must add text to search!! 🙃');
-			const projects = await db('user_projects')
-				.select('*')
-				.whereRaw(`LOWER(name) LIKE ?`, [ `%${projectText}%` ])
-				.andWhere('privateProjects', false);
+  async updateCategory(_, { data }) {
+    const category = await db('project_categories').update(data).returning('*');
+    console.log(category);
+    return category[0];
+  },
 
-			const users = await db('users')
-				.select('*')
-				.whereRaw(`LOWER(username) LIKE ?`, [ `%${userText}%` ])
-				.orWhereRaw(`LOWER(CONCAT("firstName", "lastName")) LIKE ?`, [ `%${userText}%` ]);
-			return { projects, users };
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-	},
+  async deleteCategory(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deleteCategory = await db('project_categories')
+        .where('id', id)
+        .del();
+      if (!deleteCategory) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addCategory(_, { data }) {
-		return await db('project_categories').insert(data).return('*');
-	},
+  async addUserResearch(_, { data }) {
+    const userResearch = await db('user_research').insert(data).returning('*');
+    console.log(userResearch);
+    return userResearch[0];
+  },
 
-	async updateCategory(_, { data }) {
-		return await db('project_categories').update(data).return('*');
-	},
+  async addUserResearching(_, { data }) {
+    const userResearch = await db('user_research').insert(data).returning('*');
+    console.log(userResearch);
+    return userResearch[0];
+  },
 
-	async deleteCategory(_, { id }) {
-		const deletedCategory = await db('invite').del().where('id', id);
-		try {
-			if (!deletedCategory) {
-				throw new Error('This category does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async deleteUserResearch(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deleteResearch = await db('user_research').where('id', id).del();
+      if (!deleteResearch) return rej(false);
+      return res(true);
+    });
+  },
 
-	async addUserResearch(_, { data }) {
-		return await db('user_research').insert(data).return('*');
-	},
+  async addProjectInvite(_, { data }) {
+    const projectInvite = await db('project_teams').insert(data).returning('*');
+    console.log(projectInvite);
+    return projectInvite[0];
+  },
 
-	async addUserResearching(_, { data }) {
-		return await db('user_research').insert(data).return('*');
-	},
+  async updateProjectInvite(_, { data }) {
+    const projectInvite = await db('project_teams').update(data).returning('*');
+    console.log(projectInvite);
+    return projectInvite[0];
+  },
 
-	async deleteUserResearch(_, { id }) {
-		const deletedUserResearch = await db('user_research').del().where('id', id);
-		try {
-			if (!deletedUserResearch) {
-				throw new Error('This research does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	},
+  async updateProjectInvites(_, { data }) {
+    const projectInvite = await db('project_teams').update(data).returning('*');
+    console.log(projectInvite);
+    return projectInvite[0];
+  },
 
-	async addProjectInvite(_, { data }) {
-		return await db('project_teams').insert(data).return('*');
-	},
-
-	async updateProjectInvite(_, { data }) {
-		return await db('project_teams').update(data).return('*');
-	},
-
-	async updateProjectInvites(_, { data }) {
-		return await db('project_teams').update(data).return('*');
-	},
-
-	async deleteProjectInvite(_, { id }) {
-		const deletedProjectInvite = await db('user_research').del().where('id', id);
-		try {
-			if (!deletedProjectInvite) {
-				throw new Error('This research does not exist! 😕');
-			}
-		} catch (err) {
-			console.log(err);
-			return err;
-		}
-		return true;
-	}
+  async deleteProjectInvite(_, { id }) {
+    return new Promise(async (res, rej) => {
+      const deleteProject = await db('project_teams').where('id', id).del();
+      if (!deleteProject) return rej(false);
+      return res(true);
+    });
+  },
 };
 
 module.exports = { Mutation };
--- schema/Mutation.js
+++ schema/Mutation.js
@@ -3,20 +3,17 @@ const { gql } = require('apollo-server-express');
 const mutationTypes = gql`
   type Mutation {
     addUser(data: UserInput!): User!
-    updateUser(data: UserInput!): User!
+    updateUser(data: updateUserInput!): User!
     deleteUser(id: ID!): Boolean!
     addProject(data: ProjectInput!): Projects!
-    updateProject(data: ProjectInput!): Projects!
+    updateProject(data: updateProjectInput!): Projects!
     deleteProject(id: ID!): Boolean!
     addProjectPhoto(data: ProjectPhotoInput!): Project_photos!
-    updateProjectPhoto(data: ProjectPhotoInput!): Project_photos!
+    updateProjectPhoto(data: updateProjectPhotoInput!): Project_photos!
     deleteProjectPhotos(id: ID!): Boolean!
     addComments(data: CommentsInput!): Comments!
-    updateComments(data: CommentsInput!): Comments!
+    updateComments(data: updateCommentsInput!): Comments!
     deleteComments(id: ID!): Boolean!
-    addPhotoComments(data: CommentsInput!): Comments!
-    updatePhotoComments(data: CommentsInput!): Comments!
-    deletePhotoComments(id: ID!): Boolean!
     addFollower(data: addFollowerInput!): Followers!
     deleteFollower(id: ID!): Boolean!
     addHeatmap(data: addHeatmapInput!): Heatmap!
@@ -27,17 +24,17 @@ const mutationTypes = gql`
     addInviteFollow(data: InviteInput): Invite!
     addInviteStarred(data: InviteInput): Invite!
     addInviteComments(data: InviteInput): Invite!
-    updateInvites(data: InviteInput): Invite!
+    updateInvites(data: updateInviteInput): Invite!
     deleteInvite(id: ID!): Boolean!
     search(text: String!): Search!
     addCategory(data: CategoryInput): Categories!
-    updateCategory(data: CategoryInput): Categories!
+    updateCategory(data: updateCategoryInput): Categories!
     deleteCategory(id: ID!): Boolean!
     addUserResearch(data: UserResearchInput): User_research!
     addUserResearching(data: UserResearchInput): User_research!
     deleteUserResearch(id: ID!): Boolean!
     addProjectInvite(data: InviteInput): Project_invite!
-    updateProjectInvite(data: InviteInput): Project_invite!
+    updateProjectInvite(data: updateInviteInput): Project_invite!
     updateProjectInvites(data: InviteInput): Project_invite!
     deleteProjectInvite(id: ID!): Boolean!
   }
