0d9832a364b1cfe68ee984c7cb7e10be060d5cfa
JimmyMcBride <mcbride967@gmail.com>
2020-06-02 14:22:41 -0500

    commented things back in

--- resources/projectPhoto/photoRouter.js
+++ resources/projectPhoto/photoRouter.js
@@ -1,14 +1,14 @@
-// const express = require('express');
-// const router = express.Router();
-// const photoController = require('./photoController');
+const express = require('express');
+const router = express.Router();
+const photoController = require('./photoController');
 
-// const secured = require('../utils/secured');
+const secured = require('../utils/secured');
 
-// router.post('/signed', photoController.signedUrl);
-// // router.get('/', photoController.getAllPhotos);
-// router.get('/:id', photoController.getPhotosByProjectId);
-// router.get('/one/:id', photoController.getPhotoById);
-// router.post('/', secured, photoController.createProjectPhoto);
-// router.delete('/:id', secured, photoController.deletePhotoById);
+router.post('/signed', photoController.signedUrl);
+// router.get('/', photoController.getAllPhotos);
+router.get('/:id', photoController.getPhotosByProjectId);
+router.get('/one/:id', photoController.getPhotoById);
+router.post('/', secured, photoController.createProjectPhoto);
+router.delete('/:id', secured, photoController.deletePhotoById);
 
-// module.exports = router;
+module.exports = router;
--- resources/utils/userMatches.js
+++ resources/utils/userMatches.js
@@ -1,11 +1,19 @@
-// const go = require('./crud');
+const go = require('./crud');
 
-// const userMatches = process.env.DB_ENV === 'testing' ? () => true : async (token, userId) => {
-//   const user = await go.getById('users', userId);
-//   console.log('~~~~usermatches log. token: ', token.sub, 'USERID: ', user[0].auth0Id)
-//   if(user[0] && user[0].auth0Id === token.sub) {
-//     return true;
-//   } else return false;
-// }
+const userMatches =
+  process.env.DB_ENV === 'testing'
+    ? () => true
+    : async (token, userId) => {
+        const user = await go.getById('users', userId);
+        console.log(
+          '~~~~usermatches log. token: ',
+          token.sub,
+          'USERID: ',
+          user[0].auth0Id
+        );
+        if (user[0] && user[0].auth0Id === token.sub) {
+          return true;
+        } else return false;
+      };
 
-// module.exports = userMatches;
+module.exports = userMatches;
